88893e1e97e318794aa79ca60bcfb14eb2f074e1
Yan <yan960517@gmail.com>
2022-12-05 01:34:30 +0800

    commit week3 homework

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
 	return render_template("index.html")
 @app.route("/attraction/<id>")
 def attraction(id):
-	return render_template("attraction.html")
+	return render_template("attraction.html", id = id)
 @app.route("/booking")
 def booking():
 	return render_template("booking.html")
