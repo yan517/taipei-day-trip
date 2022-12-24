5ce63bbf0b1816725d7e35ce2f1fb6df7cb894b9
Yan <yan960517@gmail.com>
2022-12-24 15:20:09 +0800

    add app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import *
 from data.api.attraction import attractionApp
 from data.api.user import userApp
 from data.api.booking import bookingApp
+from data.api.order import orderApp
 from extensions import db,bcrypt
 from dotenv import load_dotenv
 import os
@@ -26,14 +27,17 @@ def attraction(id):
 	return render_template("attraction.html", id = id)
 @app.route("/booking")
 def booking():
-	return render_template("booking.html")
-@app.route("/thankyou")
+	return render_template("booking.html", key = os.getenv('APPKEY'), app_id = os.getenv('APPID'))
+@app.route("/thankyou", methods=["GET"])
 def thankyou():
-	return render_template("thankyou.html")
+	if 'number' in request.args:
+		number = request.args['number']
+	return render_template("thankyou.html", num = number)
 
 app.register_blueprint(attractionApp, url_prefix='')
 app.register_blueprint(userApp, url_prefix='')
 app.register_blueprint(bookingApp, url_prefix='')
+app.register_blueprint(orderApp, url_prefix='')
 
 #app.run(port=3000)		
 app.run(host='0.0.0.0',port=3000)		
